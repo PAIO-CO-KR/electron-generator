354125fe5f57f1cd1832e03211b2b293d7aaa390
cranberry <justloveher79@gmail.com>
2015-05-22 17:08:14 +0900

    add build command each platform (linux,os x, wind), each platforms has their directory

--- gulp/binaryDownload.js
+++ gulp/binaryDownload.js
@@ -17,9 +17,9 @@ function onerror (err) {
 }
 
 var paths = {
-    darwin: path.join(__dirname, '../.electrons/Electron.app/Contents/MacOS/Electron'),
-    linux: path.join(__dirname, '../.electrons/electron'),
-    win32: path.join(__dirname, '../.electrons/electron.exe')
+    darwin: path.join(__dirname, '../.electrons/osx/Electron.app/Contents/MacOS/Electron'),
+    linux: path.join(__dirname, '../.electrons/linux/electron'),
+    win32: path.join(__dirname, '../.electrons/win32/electron.exe')
 };
 
 //if (!paths[platform]) throw new Error('Unknown platform: ' + platform)
@@ -29,11 +29,11 @@ var paths = {
 
 // unzips and makes path.txt point at the correct executable
 function extractFile (err, zipPath) {
-    console.log('3');
     if (err) return onerror(err)
     fs.writeFile(path.join(__dirname, 'path.txt'), paths[platform], function (err) {
         if (err) return onerror(err)
-        extract(zipPath, {dir: path.join(__dirname, '../.electrons')}, function (err) {
+        //extract(zipPath, {dir: path.join(__dirname, '../.electrons')}, function (err) {
+        extract(zipPath, {dir: path.join(__dirname, config[platform].downloadPath)}, function (err) {
             if (err) return onerror(err)
             fileCopy(config[platform]);
         })
@@ -41,31 +41,12 @@ function extractFile (err, zipPath) {
 
 }
 
-//module.exports={
-//    //download all kind of os Binary - 모든 운영체제의 바이너리를 다운로드 한다.
-//    downloadAllBin:function downloadAllBin() {
-//        download({version: version, platform: 'win32'}, extractFile);
-//        //download({version: version, platform: 'darwin'}, extractFile);
-//        //download({version: version, platform: 'linux'}, extractFile);
-//    },
-//
-//    //download one kind of os Binary - 한종류의 바이너리를 다운로드 한다.
-//    downloadBin:function downloadBin(osType){
-//        if(osType === '' || osType != null){
-//            download({version: version, platform: osType}, extractFile(osType));
-//        }
-//    }
-//}
 function fileCopy(osType){
-    console.log('4');
-    console.log('file copy function');
     xfs.ensureDir('dist',function(err){
         if(err) return console.log(err);
-        console.log('makeDir success!');
     })
     xfs.copy(osType.filepath.toString() ,osType.destpath.toString(),'-r',function(err){
         if(err)return console.log(err);
-        console.log('electron copy done');
         //일렉트론을 다운로드 받았을 때에 리소스 파일 복사.
         appResource(osType);
     });
@@ -74,30 +55,48 @@ function fileCopy(osType){
 function appResource(osType){
     xfs.copy('./src/app',osType.resource.toString(),function(err){
         if(err)return console.log(err);
-        console.log('resource copy done');
     });
 }
 
 module.exports = function(gulp,gulpsync){
     gulp.task('download-win-binary',function(){
-        console.log('download-win-binary');
+        platform="win32";
         download({version: version, platform: 'win32'}, extractFile);
     });
 
     gulp.task('download-osx-binary',function(){
-        console.log('download-osx-binary');
+        platform="darwin";
         download({version: version, platform: 'darwin'}, extractFile);
     });
 
     gulp.task('download-linux-binary',function(){
-        console.log('download-linux-binary');
+        platform="linux";
         download({version: version, platform: 'linux'}, extractFile);
     });
 
     gulp.task('build-osx',function(){
-        console.log('1');
         runSequence('download-osx-binary',function(){
             appResource(config[platform]);
         });
     });
+
+    gulp.task('build-linux',function(){
+        runSequence('download-linux-binary',function(){
+            appResource(config[platform]);
+        });
+    });
+
+    gulp.task('build-win',function(){
+        runSequence('download-win-binary',function(){
+            appResource(config[platform]);
+        });
+    });
+
+    //gulp.task('build-all',function(){
+    //    console.log('1');
+    //    runSequence('build-osx','build-linux','build-win',function(err){
+    //       if(err)return console.log(err);
+    //        console.log('all binary build done');
+    //    });
+    //});
 }
